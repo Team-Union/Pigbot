531d7e8f043e401f98dfa2b17c9c8b2c6cfa0bf5
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-08-15 11:43:50 +0800

    Update Botinfo.js

--- command/INFO/Botinfo.js
+++ command/INFO/Botinfo.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 exports.run = async (client, msg, args, prefix) => {
     let embed = new Discord.MessageEmbed() // var -> let으로 수정하였습니다.
         .setTitle("봇정보")
-        .setDescription(`**꿀꿀봇은 <:nodejs:735724721081155594>와 :heart:로 만든 봇입니다!\n모든 문의 사항 및 제보는 [지원 서버](https://invite.gg/pigbot)를 이용해주세요.\n 항상 꿀꿀봇을 이용해주셔서 감사합니다.`)
+        .setDescription(`**꿀꿀봇은 <:nodejs:735724721081155594>와 :heart:로 만든 봇입니다!\n모든 문의 사항 및 제보는 [지원 서버](https://invite.gg/pigbot)를 이용해주세요.\n 항상 꿀꿀봇을 이용해주셔서 감사합니다.**`)
         .setColor("BLUE")
         .setFooter("꿀꿀봇")
         .setThumbnail("https://cdn.discordapp.com/attachments/729175908849549363/735719327596150864/file_1.png")
