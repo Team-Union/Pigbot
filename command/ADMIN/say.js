aa56ae602991c1a8b0160f244de57af959301e68
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-08-26 12:37:44 +0800

    Update say.js

--- command/ADMIN/say.js
+++ command/ADMIN/say.js
@@ -1,52 +1,77 @@
 const { MessageEmbed } = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-      if (!args.join(" ")) return message.channel.send("내용을 써 주세요!")
-
-      message.channel.send(new MessageEmbed().setTitle(`꿀꿀봇 공지사항`).setDescription(`\`\`\`\n${args.join(" ")}\n\`\`\``).setColor("RANDOM")).then(async th => {
-          await th.react("⭕")
-          await th.react("❌")
-            
-          th.awaitReactions((reaction, user) => (reaction.emoji.name === "❌" || reaction.emoji.name === "⭕") && user.id === message.author.id, {
-              max: 1,
-              time: 20000
-          }).then(collected => {
-              if (collected.array()[0].emoji.name === "⭕") {
-                  let result = ''
-
-                  client.guilds.cache.forEach(g => {
-                      let gc
-
-                      g.channels.cache.forEach(c => {
-                          if (c.name.includes(client.user.username) || c.name.includes("봇공지") || c.name.includes("봇-공지") || c.name.includes("봇_공지") || c.name.includes("꿀꿀봇_공지사항")) gc = c.id
-                      })
-
-                      const Ch = client.channels.cache.get(gc)
-                      let ment = ''
-
-                      try {
-                          if (!Ch.permissionsFor(g.me).has("SEND_MESSAGES")) ment = `${g.name}: 발신 실패 (메시지 발신 불가)\n`
-                          else if (!Ch.permissionsFor(g.me).has("EMBED_LINKS")) {
-                              Ch.send(`**꿀꿀봇 공지사항**\n\n${args.join(" ")}\n(${message.author.tag})`)
-                              ment = `${g.name}: 임베드 발신 실패 (발신 완료)\n`
-                          }
-                          else Ch.send(new MessageEmbed().setTitle(`꿀꿀봇 공지사항`).setThumbnail(client.user.displayAvatarURL()).setDescription(args.join(" ")).setColor(0x00ff00).setFooter(message.author.tag, message.author.displayAvatarURL()).setTimestamp())
-                      } catch (e) {
-                          if (!Ch) ment = `${g.name}: 발신 실패 (채널이 없음)\n`
-                          else ment = `${g.name}: 발신 실패 (오류: ${e})\n`
-                      } finally {
-                          if (ment) result += ment
-                      }
-                  })
-          
-                  th.edit(new MessageEmbed().setTitle("공지사항 발신 완료").setDescription(`**결과**\n\`\`\`\n${result || "모든 서버에 발신 완료"}\n\`\`\``).setColor(0x00ff00))
-              } else {
-                  th.edit(new MessageEmbed().setTitle("공지사항 발신 취소").setColor(0x00ff00))
-              }
-          })
-      })
+        if (!toSend) return message.channel.send('공지 내용을 써 주세요.');
+        const prompt = new Discord.MessageEmbed()
+            .setTitle('공지를 전송할까요?')
+            .setThumbnail(client.user.displayAvatarURL())
+            .setColor('RANDOM')
+            .addField('전송할 내용', toSend, true)
+            .addField('전송자', message.author.tag, true)
+            .setFooter(message.author.tag, message.author.displayAvatarURL())
+            .setTimestamp()
+        let p = await message.channel.send(prompt);
+        p.react('✅');
+        p.react('❌');
+        const filter = (reaction, user) => (reaction.emoji.name === '✅' || reaction.emoji.name === '❌') && user.id === message.author.id;
+        const collector = p.createReactionCollector(filter, {
+            time: 30000,
+            max: 1
+        });
+        collector.on('end', async collected => {
+            if (collected && collected.first() && collected.first().emoji.name == '✅') {
+                let permissionDeniedCnt = 0;
+                const noticeEmbed = new Discord.MessageEmbed()
+                    .setTitle(`꿀꿀봇 공지`)
+                    .setColor('BLUE')
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setDescription(`${toSend}
+공지가 왜 여기로 오나요?
+꿀꿀봇 공지는 기본적으로 랜덤 채널에 발송돼요. 공지 채널을 설정하려면 채널 주제에 \`${client.user.id}\`를 포함시키거나 \`봇공지\` 또는 \`봇-공지\` 또는 \`봇_공지\`를 포함하는 이름을 가진 채널을 만들어주세요.
+🔗 [봇 초대하기](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)
+🔗 [꿀꿀봇 공식 디스코드](https://invite.gg/pigbot)
+🔗 [꿀꿀봇 추천하기](https://koreanbots.dev/bots/702857016539873372)
+[${client.user.username} 사이트](${process.env.WEBSITE})`)
+                    .setFooter(message.author.tag, message.author.displayAvatarURL())
+                    .setTimestamp();
+                prompt.setTitle(`${client.emojis.cache.find(x => x.name == 'loading')} 공지 전송 중...`)
+                    .setColor('RANDOM')
+                    .setTimestamp();
+                await p.edit(prompt);
+                for (let x of client.guilds.cache.array()) {
+                  if (x.channels.cache.find(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS']) && c.topic && c.topic.includes(client.user.id))) {
+                    x.channels.cache.find(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS']) && c.topic && c.topic.includes(client.user.id)).send(noticeEmbed);
+                  } else if (x.channels.cache.find(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS']) && (c.name.includes('봇공지') || c.name.includes('봇-공지') || c.name.includes('봇_공지')))) {
+                    x.channels.cache.find(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS']) && (c.name.includes('봇공지') || c.name.includes('봇-공지') || c.name.includes('봇_공지'))).send(noticeEmbed);
+                  } else if (x.channels.cache.find(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS']))) {
+                    x.channels.cache.filter(c => (c.type == 'text' || c.type == 'news') && c.permissionsFor(client.user).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'])).random().send(noticeEmbed);
+                  } else {
+                    permissionDeniedCnt++;
+                  }
+                }
+                prompt.setTitle(`공지 전송 완료!`)
+                  .spliceFields(0, 2)
+                  .addField('전송한 내용', toSend)
+                  .addField('전송한 서버 수', client.guilds.cache.size - permissionDeniedCnt)
+                  .addField('전송하지 못한 서버 수', permissionDeniedCnt)
+                  .addField('전송자', message.author.tag)
+                  .setColor('RED')
+                  .setTimestamp()
+              p.edit(prompt);
+            } else {
+                const cancled = new Discord.MessageEmbed()
+                    .setTitle('공지 전송이 취소되었어요.')
+                    .setColor(0xff0000)
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .addField('전송 예정이었던 내용', toSend, true)
+                    .setFooter(message.author.tag, message.author.displayAvatarURL())
+                    .setTimestamp()
+                p.edit(cancled);
+            }
+            p.reactions.removeAll();
+        });
+    }
 }
-
 
 exports.config = {
   name: "공지",
