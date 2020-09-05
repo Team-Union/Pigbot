e3882cf9a61eb6b3eaea877248290cfc52040eba
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-09-05 22:12:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs')
+const { readdirSync } = require('fs')
 const config = require("./config.json")
 
 client.on("warn", console.warn);
