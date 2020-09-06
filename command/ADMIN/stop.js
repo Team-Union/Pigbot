fab6dda434ad7c27eb8149e7c24622c6ba534616
MGL-sujan <anhgerelanhaa87@gmail.com>
2020-09-06 14:31:26 +0800

    Update

--- command/ADMIN/stop.js
+++ command/ADMIN/stop.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, prefix) => {
-  if (!client.devs.includes(msg.author.id))
+  if (!client.devs.includes(message.author.id))
     return message.reply("이 명령어는 꿀꿀봇 관리자만 사용할 수 있습니다.");
     try {
       await message.channel.send("꿀꿀봇 종료중...");
