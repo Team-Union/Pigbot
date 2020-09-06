8aca9f7724ed6be6c7331b904ddcee24c6b9860f
MGL-sujan <anhgerelanhaa87@gmail.com>
2020-09-06 15:56:48 +0800

    Update

--- command/INFO/updatedlog.js
+++ command/INFO/updatedlog.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js")
 exports.run = async (client, msg, args, prefix) => {
     let embed = new Discord.MessageEmbed() // var -> let으로 수정하였습니다.
-        .setTitle("V2.3.2 업데이트 안내")
-        .setDescription("```diff\n+ 필요 없는 모듈 제거\n+ 봇정보 안정화!\n-뮤트 명령어 삭제```")
+        .setTitle("V2.4.1 업데이트 안내")
+        .setDescription("```diff\n+ 노래시스탬 추가\n+ 서버정보 안정화!```\n🔗[꿀꿀봇 초대하기](https://discord.com/api/oauth2/authorize?client_id=702857016539873372&permissions=8&scope=bot)\n🔗[지원서버 들어가기](https://invite.gg/pigbot)\n🔗 [꿀꿀봇 추천하기](https://koreanbots.dev/bots/702857016539873372)")
         .setColor("BLUE")
         .setFooter("Updated V2.3.2")
         .setThumbnail("https://media.discordapp.net/attachments/729175908849549363/735719327596150864/file_1.png?width=442&height=442")
