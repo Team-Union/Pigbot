638bd7a845f3037afd4d4bb2603764f5ac009283
MGL-sujan <anhgerelanhaa87@gmail.com>
2020-09-06 15:42:28 +0800

    Update

--- command/MUSIC/play.js
+++ command/MUSIC/play.js
@@ -8,11 +8,9 @@ if (!queue) return message.channel.send({embed: {color: client.colors.error, des
 let playing = client.player.isPlaying(message.guild.id);
 
 if(playing){
-    // Add the song to the queue
     let song = await client.player.addToQueue(message.guild.id, queue);
     message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.success} | ${song.name} 을 대기열에 추가했어요!` }})
 } else {
-    // Else, play the song
     let song = await client.player.play(message.member.voice.channel, queue);
     message.channel.send({embed: {color: client.colors.success, description: `${client.emotes.music} | 현재 재생중인 곡:\n${song.name}` }})
     song.queue.on('end', () => {
