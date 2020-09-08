aebd0062bac3402a2dbea710fbc331a10ad43d5a
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-09-08 18:33:17 +0800

    Update exec.js

--- command/ADMIN/exec.js
+++ command/ADMIN/exec.js
@@ -1,6 +1,7 @@
-if (!author.includes(message.author.id)) return message.reply({embed:{title:`${client.emojis.cache.find(x=> x.name == "no")}권한을 확인하니, 당신은 \`User(일반유저)\`권한을 가지고잇어요`,description:' **``dev(개발자)``** 권한이 필요합니다',color:"RED"}})
-             if(!args.length) {
-const embed = new MessageEmbed()
+const Discord = require("discord.js");
+exports.run = async (client, message, args, prefix) => {
+  if (!client.devs.includes(message.author.id))
+    return message.reply("이 명령어는 꿀꿀봇 관리자만 사용할 수 있습니다."); // bot.js에서 client.devs를 저장한 것을 불러와 포함하지 않으면 해당 메세지로 답변해줍시다.
 .setTitle('**[오류]**')
 .setDescription(`**[개발자]**${message.author}/${message.author.id}님 앞에 글자를 입력해야죠!`) 
 .setColor("RED")
