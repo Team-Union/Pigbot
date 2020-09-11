75b6903293dcde2948e41a96f44b0f0c9be1a396
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-09-11 18:40:52 +0800

    Update announce.js

--- command/ADMIN/announce.js
+++ command/ADMIN/announce.js
@@ -16,12 +16,12 @@ if (!client.devs.includes(message.author.id))
             }).then(collected => {
                 if (collected.array()[0].emoji.name === "⭕") {
                     let result = '';
-th.edit(new MessageEmbed().setTitle('📡공지가 전송되었습니다.').setColor('GREEN'))
+th.edit(new MessageEmbed().setTitle('📡공지가 전송되었습니다.').setColor('BLUE'))
                     client.guilds.cache.forEach(g => {
                         let gc;
 
                         g.channels.cache.forEach(c => {
-                            if (c.name.includes("봇-공지")||c.name.includes("봇_공지") || c.name.includes("봇-공지사항") || c.name.includes("botnotice") || c.name.includes("봇공지") || c.name.includes("꿀꿀붓_공지사항") || c.name.includes("봇_공지") || c.name.includes('🤖봇-공지')|| c.name.includes('📜ㅣ공지사항')||c.name.includes('  ')) gc = c.id;
+                            if (c.name.includes("봇-공지")||c.name.includes("봇_공지") || c.name.includes("봇-공지사항") || c.name.includes("📢봇-공지") || c.name.includes("봇공지") || c.name.includes("꿀꿀붓_공지사항") || c.name.includes("봇_공지") || c.name.includes('🤖봇-공지')|| c.name.includes('📜ㅣ공지사항')||c.name.includes('  ')) gc = c.id;
                         
                         
                         });
@@ -34,7 +34,7 @@ th.edit(new MessageEmbed().setTitle('📡공지가 전송되었습니다.').setC
                             })
                             if (!Ch.permissionsFor(g.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES', 'EMBED_LINKS'])) return message.author.send(`${g.name}: 발신 실패 (메시지 발신 실패)\n`)
                             
-                            Ch.send(new MessageEmbed().setTitle(`${client.user.username} 공지`).setThumbnail(client.user.displayAvatarURL()).setDescription(args.join(" ")+"\n왜 이공지가 여기서 나오냐고요?\n이름을 `봇공지, 봇-공지, 봇_공지, 꿀꿀봇_공지사항` 설정시 뜸니다\n🔗 [꿀꿀봇 공식 디스코드](https://invite.gg/pigbot)\n🔗 [꿀꿀봇 추천하기](https://koreanbots.dev/bots/702857016539873372)n\🔗 [꿀꿀봇 Github](https://github.com/Pigbot-Official/)\n🔗 [꿀꿀봇 초대하기](https://discordapp.com/oauth2/authorize?client_id=702857016539873372&scope=bot&permissions=8)").setColor(0x00ff00).setFooter(message.author.tag, message.author.displayAvatarURL()).setTimestamp())
+                            Ch.send(new MessageEmbed().setTitle(`꿀꿀봇 공지사항`).setThumbnail(client.user.displayAvatarURL()).setDescription(args.join(" ")+"\n \n왜 이공지가 여기서 나오냐고요?\n이름을 `봇공지, 봇-공지, 봇_공지, 꿀꿀봇_공지사항` 설정시 뜸니다\n \n🔗 [꿀꿀봇 공식 디스코드](https://invite.gg/pigbot)\n🔗 [꿀꿀봇 추천하기](https://koreanbots.dev/bots/702857016539873372)\n🔗 [꿀꿀봇 Github](https://github.com/Pigbot-Official/)\n🔗 [꿀꿀봇 초대하기](https://discordapp.com/oauth2/authorize?client_id=702857016539873372&scope=bot&permissions=8)").setColor(0x00ff00).setFooter(message.author.tag, message.author.displayAvatarURL()).setTimestamp())
                         } catch (e) {
                             message.author.send(`에러남.\n${e.message || e}`)
                         }
